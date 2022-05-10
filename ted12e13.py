3e92bb03c92febca4631331b0419773cc11a4e74
DiogoBruno06 <diogobruno06@outlook.com>
2022-05-09 23:44:05 -0300

    first commit

---
+++ ted12e13.py
@@ -0,0 +1,11 @@
+glossario = {
+    'Python':'É uma linguagem de programação de alto nível, interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.',
+    'Algoritmo':'É simplesmente uma "receita" para executarmos uma tarefa ou resolver algum problema',
+    'WebScraping':'A coleta de dados web, ou raspagem web, é uma forma de mineração que permite a extração de dados de sites da web convertendo-os em informação estruturada para posterior análise.',
+    'Visualstudiocode':'É um editor de código-fonte desenvolvido pela Microsoft para Windows, Linux e macOS.',
+    'PyCharm':'É um ambiente de desenvolvimento integrado usado em programação de computadores, especificamente para a linguagem de programação Python',
+}
+for i, x in glossario.items():
+ print ('A palavra {}, {}'.format(i,x))
+ print("-"*50)
+ 
